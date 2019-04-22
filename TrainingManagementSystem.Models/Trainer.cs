fff4f5fc7897ce6a80a5479b4bbe1364c6081979
DIPON KUMAR DAS <37617060+dipon44@users.noreply.github.com>
2019-04-22 17:30:58 +0600

    Created Trainer Model

---
+++ TrainingManagementSystem.Models/Trainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingManagementSystem.Models
+{
+    public class Trainer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
