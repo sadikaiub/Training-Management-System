df782d7676fe258da276bcda8ba3977e14ea1207
sadikaiub <rafiqulislamsadik55@gmail.com>
2019-04-22 20:00:34 +0600

    created course model

---
+++ TrainingManagementSystem.Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingManagementSystem.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+        
+    }
+}
