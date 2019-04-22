60318f20c358e96623ddc4193135410998acc1de
sadikaiub <rafiqulislamsadik55@gmail.com>
2019-04-22 17:08:06 +0600

    Created TrainingManagementSystem Repository

---
+++ TrainingManagementSystem.Models/Trainee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingManagementSystem.Models
+{
+    public class Trainee
+    {
+        public int Id { get; set; }
+        public string Name { set; get; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+        public string Address { get; set; }
+
+    }
+}
