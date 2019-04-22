c5207f85b27a946aa1c3276d331a47bc8e2d6232
DIPON KUMAR DAS <37617060+dipon44@users.noreply.github.com>
2019-04-22 19:12:52 +0600

    Admin and Executive Model Created

---
+++ TrainingManagementSystem.Models/Admin.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainingManagementSystem.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ TrainingManagementSystem.Models/Executive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingManagementSystem.Models
+{
+  public class Executive
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+}
