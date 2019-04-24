be97e3ac802935e7df4e3b271c4d0e5ed01e77dc
Rajesh-Kumar-Roy <rajeshkumarroy01998@gmail.com>
2019-04-24 23:39:36 +0600

    AdminRepository IAdminRepository BatchRepository IBatchRepository Added

---
+++ TrainingManagementSystem.Repositories/AdminRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TrainingManagementSystem.Models;
+using TrainingManagementSystem.Repositories.Base;
+using TrainingManagementSystem.Repositories.Contracts;
+
+namespace TrainingManagementSystem.Repositories
+{
+    public class AdminRepository:BaseRepository<Admin>,IAdminRepository
+    {
+        public AdminRepository(DbContext db):base(db)
+        {
+            
+        }
+    }
+}
---
+++ TrainingManagementSystem.Repositories/BatchRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TrainingManagementSystem.Models;
+using TrainingManagementSystem.Repositories.Base;
+using TrainingManagementSystem.Repositories.Contracts;
+
+namespace TrainingManagementSystem.Repositories
+{
+    public class BatchRepository:BaseRepository<Batch>,IBatchRepository
+    {
+        public BatchRepository(DbContext db):base(db)
+        {
+            
+        }
+    }
+}
